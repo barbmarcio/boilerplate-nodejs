42282b3619a9154c377bec5d87f558c41b75feed
Marcio Barbosa <marcio.barbosa@freddiesflowers.com>
2023-09-27 11:56:48 -0300

    fix: reverting changes

--- src/modules/authentication/services/authentication.service.ts
+++ src/modules/authentication/services/authentication.service.ts
@@ -20,7 +20,6 @@ class AuthenticationService {
     data: AuthenticationDTO,
     action: ActionType,
   ): Promise<string> {
-    const abc = '';
     const foundUser = await this.authRepository.getUserByEmail(data.email);
 
     if (!foundUser) {
